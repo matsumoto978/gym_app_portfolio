7fbf247acfe19e54095c8816e8e28589c6c5a770
matsumoto978 <myoshi878@gmail.com>
2021-10-06 16:15:43 +0900

    ストロングパラメーターの追加

--- db/schema.rb
+++ db/schema.rb
@@ -26,5 +26,4 @@ ActiveRecord::Schema.define(version: 2021_10_06_065510) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
