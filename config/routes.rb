1e0e166ea2b77a4a86bd983f9e96e3eec18ff82e
matsumoto978 <myoshi878@gmail.com>
2021-10-12 00:32:08 +0900

    ゲストユーザーのメールアドレス・パスワードを変更できないようにしてメール送信機能の停止

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root to: "gyms#index"
+  devise_for :users, controllers: {
+                       registrations: "users/registrations",
+                       passwords: "users/passwords",
+                     }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
-  devise_for :users
   resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
