90ff299a1c97df535a8a1591d2c8ddc352113967
matsumoto978 <myoshi878@gmail.com>
2021-10-16 00:41:08 +0900

    commentモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_153420) do
+ActiveRecord::Schema.define(version: 2021_10_15_153553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 2021_10_15_153420) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "gym_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gym_id"], name: "index_comments_on_gym_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "gyms", force: :cascade do |t|
@@ -90,6 +100,8 @@ ActiveRecord::Schema.define(version: 2021_10_15_153420) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "gyms"
+  add_foreign_key "comments", "users"
   add_foreign_key "gyms", "users"
   add_foreign_key "keeps", "gyms"
   add_foreign_key "keeps", "users"
