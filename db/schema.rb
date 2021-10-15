893180559a7f1976d285f5e5749dc0cde7efa801
matsumoto978 <myoshi878@gmail.com>
2021-10-16 00:35:28 +0900

    likeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_153205) do
+ActiveRecord::Schema.define(version: 2021_10_15_153420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,16 @@ ActiveRecord::Schema.define(version: 2021_10_15_153205) do
     t.index ["user_id"], name: "index_keeps_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "gym_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gym_id"], name: "index_likes_on_gym_id"
+    t.index ["user_id", "gym_id"], name: "index_likes_on_user_id_and_gym_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -83,4 +93,6 @@ ActiveRecord::Schema.define(version: 2021_10_15_153205) do
   add_foreign_key "gyms", "users"
   add_foreign_key "keeps", "gyms"
   add_foreign_key "keeps", "users"
+  add_foreign_key "likes", "gyms"
+  add_foreign_key "likes", "users"
 end
