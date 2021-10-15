42de78cc140f8ed1a0e09206f31933b54d4042d1
matsumoto978 <myoshi878@gmail.com>
2021-10-16 00:02:44 +0900

    gymモデルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_145418) do
+ActiveRecord::Schema.define(version: 2021_10_15_145935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_10_15_145418) do
   end
 
   create_table "gyms", force: :cascade do |t|
-    t.integer "user"
+    t.integer "user_id"
     t.string "gym_name"
     t.float "latitude"
     t.float "longitude"
