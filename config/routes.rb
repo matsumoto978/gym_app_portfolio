1e7696715d00bf17a3fda4038dbb0050fa17f018
matsumoto978 <myoshi878@gmail.com>
2021-10-17 23:40:51 +0900

    いいね機能のルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
   resources :users, only: :show
+  resources :gyms do
+    resource :likes, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
