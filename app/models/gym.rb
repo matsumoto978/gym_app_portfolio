1ea16fe0fab26a2b3821c8c51eeb231a4f551b92
matsumoto978 <myoshi878@gmail.com>
2021-10-18 21:20:19 +0900

    gymの詳細viewsとja.ymlの作成

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -2,6 +2,7 @@ class Gym < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
+  has_rich_text :content
 
   def liked_by?(user)
     likes.exists?(user_id: user.id)
